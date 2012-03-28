3bb96f5c268d5053837d7180463d3d282aaf0d59
bigfleet <jim@jimvanfleet.com>
2012-03-28 16:02:53 -0400

    Bringing in code from Jay's version, preparing for desired refactor

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,37 @@
 class Accountable::Account < ActiveRecord::Base 
 
 	self.table_name = "accounts"
+
+  has_many :balances
+
+  class << self
+    attr_accessor :owner_type
+  end
+
+  def self.owned_by(klass)
+    @owner_type = klass.to_s.classify.constantize
+    belongs_to :owner, :polymorphic => true
+    validate :check_owner_type
+  end
+
+  def balance_at(date)
+    balance = Balance.find_by_account_id_and_evaluated_at(id, date)
+    balance ||= Balance.new(:account => self, :evaluated_at => date)
+  end
+
+  def balance_before(date)
+    balances.find :first, :conditions => ["evaluated_at < ?", date],
+                          :order => "evaluated_at DESC"
+  end
+
+  def current_balance
+    balance_at(Time.now)
+  end
+
+private
+  def check_owner_type
+    errors.add_to_base "owner must be an #{self.class.owner_type}" if
+      self.class.owner_type and !(owner.is_a? self.class.owner_type)
+  end
 	
 end
