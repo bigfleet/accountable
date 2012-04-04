5c701127675b86ebeb3e14feaf2e2f08829584de
bigfleet <jim@jimvanfleet.com>
2012-04-04 10:01:58 -0400

    Debit specs passing

--- spec/models/debit_spec.rb
+++ spec/models/debit_spec.rb
@@ -5,5 +5,26 @@ describe Debit do
   subject { build(:debit) }
 
   it { should be_valid }
+
+  let(:debit){ build(:debit) }  
+
+  describe "validations" do
+
+    it "should not permit positive debits" do
+      debit.amount = 11.00
+      debit.should_not be_valid
+    end
+
+    it "must require an accompanying credit" do
+      lambda {
+        debit.transaction.credit = nil
+      }.should raise_error(ActiveRecord::RecordNotSaved)
+    end
+
+  end  
+
+  it "should be balanced by default" do
+    debit.should be_balanced
+  end
 	
 end
