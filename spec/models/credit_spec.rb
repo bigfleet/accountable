375bccbf8895636fdfbf919f9df2257c62a56e4f
bigfleet <jim@jimvanfleet.com>
2012-04-04 09:33:53 -0400

    Credit specs passing

--- spec/models/credit_spec.rb
+++ spec/models/credit_spec.rb
@@ -7,5 +7,24 @@ describe Credit do
   it { should be_valid }
 
   let(:credit){ build(:credit) }
+
+  describe "validations" do
+
+    it "should not permit negative credits" do
+      credit.amount = -11.00
+      credit.should_not be_valid
+    end
+
+    it "must require an accompanying debit" do
+      lambda {
+        credit.transaction.debit = nil
+      }.should raise_error(ActiveRecord::RecordNotSaved)
+    end
+
+    it "must required accompanying a debit of identical amount" do
+      credit.transaction.debit.amount = -20.00
+      credit.should_not be_valid
+    end
+  end
 	
 end
