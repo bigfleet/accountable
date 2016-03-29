25348c32080f8cacfbf54aa03f88abd6732c2f46
Jim Van Fleet <jim.van.fleet@lattapartners.com>
2016-03-29 11:38:23 -0400

    Specifying keys, all specs passing

--- app/models/summary_account.rb
+++ app/models/summary_account.rb
@@ -1,5 +1,6 @@
 class SummaryAccount < Account
-  has_and_belongs_to_many :accounts, :join_table => :account_joins
+  has_and_belongs_to_many :accounts, :join_table => :account_joins,
+    foreign_key: :summary_account_id, association_foreign_key: :detail_account_id
   has_many :entries, :through => :accounts
   validate :no_recursion
 
